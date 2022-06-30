ac0a6ba7de6071d50b35b9a5f1fb975458b6b608
hamitmizrak <hamitmizrak@gmail.com>
2022-06-30 10:40:45 +0300

    permission

--- src/main/java/com/hamitmizrak/_35_FileIO3Permission.java
+++ src/main/java/com/hamitmizrak/_35_FileIO3Permission.java
@@ -2,22 +2,27 @@ package com.hamitmizrak;
 
 import java.io.File;
 
+enum permission {
+	ADMIN, WRITER, USER;
+}
+
 public class _35_FileIO3Permission {
 	public static void main(String[] args) {
 		File file = new File(_35_FileIO.MY_PATH);
-		boolean data = file.canExecute();
-		if (data) {
-			System.out.println("Çalýþma izni verilmiþtir");
-		} else {
-			System.out.println("Çalýþma izni verilmemiþtir !!!!");
-		}
 		
-		data = file.setExecutable(false);
-		if (data) {
-			System.out.println("Çalýþma izni verilmiþtir");
-		} else {
-			System.out.println("Çalýþma izni verilmemiþtir !!!!");
-		}
+		// String[] permissionRoles = { "ADMIN", "WRITER", "USER" };
+		// if(dizileye permissionRoles[1] == "ADMIN")
 		
+		if (permission.ADMIN.toString() == "ADMIN") {
+			boolean data = file.canExecute();
+			data = file.setExecutable(false);
+			if (data) {
+				System.out.println("Çalýþma izni verilmiþtir");
+			} else {
+				System.out.println("Çalýþma izni verilmemiþtir !!!!");
+			}
+		} else {
+			System.out.println("Writer veya user dosya deðiþtirme izini yoktur.");
+		}
 	}
 }
