3edc698d7851de2b1a4e28d75d55eb117aa4fd26
hamitmizrak <hamitmizrak@gmail.com>
2022-07-01 10:03:43 +0300

    TCP/IP UDP RMI

---
+++ src/main/java/com/hamitmizrak/_36_Socket_Programming.java
@@ -0,0 +1,38 @@
+package com.hamitmizrak;
+
+public class _36_Socket_Programming {
+	// Daðýtýk Programlama (distributed System) :Ayný networkteki bilgisayarlarýn
+	// birbiriyle haberleþmesine denilir.
+	// Senkron : Ayný anda tek bir iþlem yapabilme yeteneðidir.
+	// Senkron : Ayný anda birden fazla iþlem yapabilme yeteneðidir.
+	// TCP/IP UDP (Client:istemci(Hizmet alan) Server:Hizmet veren :
+	// TCP/IP ~ UDP ==> Aðdaki Bilgisayarlarýn birbiriyle haberleþmesini saðlayan
+	// protokol adýdýr.
+	
+	// TCP/IP ile UDP arasýndaki farklar
+	// TCP/IP: güvenli ,karþý tarafa data gidip gitmediðini kontrol eder. Yavaþtýr.
+	// UDP:güvensizdir,karþý tarafa data gidip gitmediðini kontrol etmeezzzzzz.
+	// Hýzlýdýr. Canlý Live Stream (Online system)
+	
+	// RMI: Remote Method Invocation: Farklý sunuculardaki metotlarý çaðýrýp iþlem
+	// yapýlmasý durumudur.
+	
+	// 2^16=65536 port sayýsý (Kapý)
+	// 0<=X<=1023 (Well-Know-Port)
+	// SSH:22
+	// FTP:20
+	// HTTP:80, 443
+	
+	// 1024<=Y<=49151 ( Registered Port):sabitlenmiþ
+	// Mongo: 27017
+	// Postgresql:5432
+	// Mysql:3306
+	
+	// 49152<=X<=65535 Dynamic Port
+	
+	public static void main(String[] args) {
+		System.out.println(Math.pow(2, 16)); // 65536 port
+		//
+	}
+	
+}
