d1ac32aaf99a9cfc619ebc9587e388f62a2434be
hamitmizrak <hamitmizrak@gmail.com>
2022-07-01 12:17:01 +0300

    Socket programming

---
+++ src/main/java/com/hamitmizrak/_36_Socket_Programming3_twoway_x1_String_Server.java
@@ -0,0 +1,53 @@
+package com.hamitmizrak;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+// Two Way (Clint -Server String examples)
+// Client Kullanýcýdan aldýðý veriyi Servera göndersin
+// Client -Server yazýþmasýný File Yazdýrsýn.
+
+// Unutma:
+// 1-)öncelikle Server'ý çalýþtýrmalýsýn. Sonra Client'i çalýþtýrýyoruz.
+// 2-) PSVM ==> Serverda olur.
+// 3-) java.net kütüphanesini import ediyoruz
+
+public class _36_Socket_Programming3_twoway_x1_String_Server {
+	
+	public static void main(String[] args) {
+		String receiveMessage;// mesaj almak
+		String sendMessage; // mesa j göndersin
+		System.out.println("Server Hazýr");
+		
+		try {
+			ServerSocket serverSocket = new ServerSocket(8585);
+			Socket socket = serverSocket.accept();
+			
+			OutputStream outputStream = socket.getOutputStream();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			
+			PrintWriter printWriter = new PrintWriter(outputStream, true);
+			
+			InputStream inputStream = socket.getInputStream();
+			BufferedReader receiveRead = new BufferedReader(new InputStreamReader(inputStream));
+			
+			while (true) {
+				if ((receiveMessage = receiveRead.readLine()) != null) {
+					System.out.println("CLIENT: " + receiveMessage);
+				}
+				sendMessage = bufferedReader.readLine();
+				printWriter.println(sendMessage);
+				printWriter.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ src/main/java/com/hamitmizrak/_36_Socket_Programming3_twoway_x2_String_Client.java
@@ -0,0 +1,55 @@
+package com.hamitmizrak;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+// Two Way (Clint -Server String examples)
+// Client Kullanýcýdan aldýðý veriyi Servera göndersin
+// Client -Server yazýþmasýný File Yazdýrsýn.
+
+// Unutma:
+// 1-)öncelikle Server'ý çalýþtýrmalýsýn. Sonra Client'i çalýþtýrýyoruz.
+// 2-) PSVM ==> Serverda olur.
+// 3-) java.net kütüphanesini import ediyoruz
+
+public class _36_Socket_Programming3_twoway_x2_String_Client {
+	
+	public static void main(String[] args) {
+		String receiveMessage;// mesaj almak
+		String sendMessage; // mesa j göndersin
+		
+		try {
+			Socket socket = new Socket("localhost", 8585);
+			
+			// Client veri gönderecek
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			OutputStream outputStream = socket.getOutputStream();
+			PrintWriter printWriter = new PrintWriter(outputStream, true);
+			
+			// serverdan gelen veriyi almak
+			InputStream inputStream = socket.getInputStream();
+			BufferedReader bufferedReader2 = new BufferedReader(new InputStreamReader(inputStream));
+			System.out.println("Client: Lütfen mesaj yazýnýz ");
+			
+			while (true) {
+				// clientten veri almak
+				sendMessage = bufferedReader.readLine();
+				printWriter.println(sendMessage);
+				printWriter.flush();
+				
+				if ((receiveMessage = bufferedReader2.readLine()) != null) {
+					System.out.println("SERVER: " + receiveMessage);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
