9324a363c628896e00b4d3fabea489755faa2baf
hamitmizrak <hamitmizrak@gmail.com>
2022-07-06 09:38:40 +0300

    Bean

---
+++ src/main/java/com/hamitmizrak/_37_OOP_x2_Bean.java
@@ -0,0 +1,27 @@
+package com.hamitmizrak;
+
+// POJO: Plain Old Java Object
+public class _37_OOP_x2_Bean {
+	
+	// Nesne Deðiþkenleri
+	private String name;
+	private String surname;
+	
+	// getter and setter
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+}
