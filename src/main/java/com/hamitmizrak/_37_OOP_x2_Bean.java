a687d24b4d1d262be1fed5ef010b2ed6ad887be8
hamitmizrak <hamitmizrak@gmail.com>
2022-07-06 09:39:57 +0300

    pojo bean

--- src/main/java/com/hamitmizrak/_37_OOP_x2_Bean.java
+++ src/main/java/com/hamitmizrak/_37_OOP_x2_Bean.java
@@ -1,11 +1,61 @@
 package com.hamitmizrak;
 
-// POJO: Plain Old Java Object
+import java.util.Objects;
+
+// BEAN= POJO: Plain Old Java Object +constructor+seriable+toString
 public class _37_OOP_x2_Bean {
+	
+	// Javada bütün classlarýn atasý Object
 	
 	// Nesne Deðiþkenleri
 	private String name;
 	private String surname;
+	
+	// parametresiz construcor
+	public _37_OOP_x2_Bean() {
+		
+	}
+	
+	// parametreli constructor
+	public _37_OOP_x2_Bean(String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+	
+	// Overloading
+	public _37_OOP_x2_Bean(String surname) {
+		this.surname = surname;
+	}
+	
+	// toString() => bu class new ile oluþturduðumuzda classý çaðýrdýðýmýzda veriler
+	// hepsi gelir
+	@Override
+	public String toString() {
+		return "_37_OOP_x1_POJO [name=" + name + ", surname=" + surname + "]";
+	}
+	
+	// HashCode: her bir classýn sadece kendisine ait (unique) bir deðerine vardýr.
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, surname);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		_37_OOP_x2_Bean other = (_37_OOP_x2_Bean) obj;
+		return Objects.equals(name, other.name) && Objects.equals(surname, other.surname);
+	}
+	
+	// Method -1
+	public void fullName() {
+		System.out.println(this.name + this.surname);
+	}
 	
 	// getter and setter
 	public String getName() {
