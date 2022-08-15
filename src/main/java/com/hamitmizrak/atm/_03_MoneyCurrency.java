3318d6358c6dffeb01f5888fb84ff98e58180b2c
hamitmizrak <hamitmizrak@gmail.com>
2022-08-15 10:35:22 +0300

    LocalDate

--- src/main/java/com/hamitmizrak/atm/_03_MoneyCurrency.java
+++ src/main/java/com/hamitmizrak/atm/_03_MoneyCurrency.java
@@ -2,28 +2,24 @@ package com.hamitmizrak.atm;
 
 public enum _03_MoneyCurrency {
 	
-	TR(1,"tr"),EN(2,"en"),GE(3,"ge")
+	TR(1, "tr"), EN(2, "en"), GE(3, "ge");
 	
 	private final int key;
 	private final String value;
 	
-	//private constructor: new instance oluþturulmasýn
+	// private constructor: new instance oluþturulmasýn
 	private _03_MoneyCurrency(int key, String value) {
 		this.key = key;
 		this.value = value;
 	}
-//getter and setter
+	
+	// getter and setter
 	public int getKey() {
 		return key;
 	}
-
+	
 	public String getValue() {
 		return value;
 	}
-	
-	
-	
-	
-	
 	
 }
